997dc70a198ef1149d0a7cc103388ecb4e43bcf7
serig85 <115493415+serig85@users.noreply.github.com>
2024-06-29 21:45:34 +0300

    The current date has been added to the file name.

--- Python+SQL/main.py
+++ Python+SQL/main.py
@@ -1,11 +1,12 @@
 import csv
+import datetime
 import cx_Oracle
 
 
-conn = cx_Oracle.connect(user="SYS", password='123', dsn='orcl1', mode = cx_Oracle.SYSDBA)
+conn = cx_Oracle.connect(user="SYS", password='123', dsn='orcl1', mode=cx_Oracle.SYSDBA)
 
-print(conn)
-with open('csv.csv', 'a') as file:
+date = datetime.date.today().strftime('%d.%m.%Y')
+with open(f'report_{date}.csv', 'a') as file:
     file_writer = csv.writer(file, delimiter=',', lineterminator="\r")
     file_writer.writerow(['first_name', 'last_name', 'Credit_Number', 'Balance'])
     cur = conn.cursor()
@@ -14,5 +15,3 @@ with open('csv.csv', 'a') as file:
                            " JOIN credit ON credit = credit.id WHERE Balance >1000"):
         print(row)
         file_writer.writerow(row)
-
-
