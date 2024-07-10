c6271633f3992a474af493f9b2a873ed72020aa5
serig85 <115493415+serig85@users.noreply.github.com>
2024-07-10 15:44:15 +0300

    Correction of the SQL query in the main file.

--- Python+SQL/main.py
+++ Python+SQL/main.py
@@ -11,8 +11,9 @@ with open(f'report_{date}.csv', 'a' , encoding='utf-8') as file:
     file_writer = csv.writer(file, delimiter=',', lineterminator="\r")
     file_writer.writerow(['first_name', 'last_name', 'Credit_Number', 'Balance'])
     cur = conn.cursor()
-    for row in cur.execute("SELECT first_name, last_name, Credit_Number, Balance "
-                "FROM (SELECT * FROM client JOIN relation on client.id = relation.client)"
-                " JOIN credit ON credit = credit.id WHERE Balance >1000"):
+    for row in cur.execute("SELECT first_name, last_name, Credit_Number, Balance FROM client "
+                           "JOIN relation on client.id = relation.client "
+                           "JOIN credit ON credit = credit.id "
+                           "WHERE Balance >1000"):
         print(row)
         file_writer.writerow(row)
